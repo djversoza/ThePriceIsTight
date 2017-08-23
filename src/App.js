b954bcd33510c3e9b4384644540386a69fcd7600
DJ Versoza <djversoza@gmail.com>
2017-08-23 12:41:28 -0700

    adding login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   constructor(){
     super();
@@ -12,16 +13,29 @@ class App extends Component {
 
   render() {
     const hello = (
-        <div className="row">
-          <h1>yooo</h1>
-        </div>
+      <nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Brand</a>
+        <a className="navbar-brand" href="#">Brand</a>
+    </div>
+
+  </div>
+</nav>
+
     );
 
     const two = (
         <h1>Two</h1>
     );
 
-    return <div className="container">{this.state.loggedin ? hello : two}</div>
+    return <div>{this.state.loggedin ? hello : two}</div>
 
   }
 }
