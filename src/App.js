71fd01186d9dc3ab4e6e17679485a2768471bd59
DJ Versoza <djversoza@gmail.com>
2017-08-30 15:24:16 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,104 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      loggedin: true,
+      loggedin: null
     };
+  };
+
+  componentWillMount(){
+    this.state.loggedin === null ? this.setState({loggedin: false}) : this.setState({loggedin: true})
+    console.log(this.state.loggedin)
   }
+
+  hellohi(){
+    this.setState({
+      loggedin: true
+    })
+  };
+
+  logout(){
+    this.setState({
+      loggedin: false
+    })
+  }
+
 
   render() {
     const hello = (
-      <nav className="navbar navbar-default">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand" href="#">Brand</a>
-        <a className="navbar-brand" href="#">Brand</a>
+    <div>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Brand</a>
+
+
+          </div>
+          <form id="signin" className="navbar-form navbar-right" role="form">
+                       <div className="input-group login">
+                           <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                           <input id="email" type="email" className="form-control" name="email" placeholder="Email Address"/>
+                       </div>
+
+                       <div className="input-group login">
+                           <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                           <input id="password" type="password" className="form-control" name="password" placeholder="Password"/>
+                       </div>
+
+                       <button type="submit" onClick={this.hellohi.bind(this)} className="btn btn-primary login">Login</button>
+                  </form>
+        </div>
+      </nav>
+
+      <div className="container">
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      <h1>yo</h1>
+      </div>
+
     </div>
+  );
 
-  </div>
-</nav>
+  const two = (
+    <div>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Brand</a>
 
-    );
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 
-    const two = (
-        <h1>Two</h1>
-    );
-
-    return <div>{this.state.loggedin ? hello : two}</div>
-
+  return <div>{this.state.loggedin ? two : hello}</div>
   }
+
 }
 
 export default App;
